1a1db0f02801f4a550f19d2f80684c572e7f46d4
Owusu Ansah Kwadwo <54241472+hacks-and-codes@users.noreply.github.com>
2022-09-07 20:58:16 +0000

    Add files via upload

---
+++ myAtmProject/AccountMenu.java
@@ -0,0 +1,43 @@
+package myAtmProject;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AccountMenu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AccountMenu window = new AccountMenu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AccountMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ myAtmProject/Verification.java
@@ -0,0 +1,43 @@
+package myAtmProject;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Verification {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Verification window = new Verification();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Verification() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
